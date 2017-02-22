9673818da524cb8246dbe68f2d1c64d7b163da6e
Lida He <lida.he@dell.com>
2017-02-22 14:40:03 -0500

    convert hosts list to comma separated with double quotes on each

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 # convert comma separated list of hosts to comma separate list with double quotes on each
 # for example, 1.1.1.1,2.2.2.2 to "1.1.1.1", "2.2.2.2"
-LOGSTASH_HOSTS=`echo $LOGSTASH_HOSTS | sed -e 's/ //g' | sed -e 's/^\|$/"/g' | sed -e 's/,/", "/g'`
-ELASTICSEARCH_HOSTS=`echo $ELASTICSEARCH_HOSTS | sed -e 's/ //g' | sed -e 's/^\|$/"/g' | sed -e 's/,/", "/g'`
+LOGSTASH_HOSTS=\"`echo $LOGSTASH_HOSTS | sed -e 's/ //g' | sed -e 's/,/", "/g'`\"
+ELASTICSEARCH_HOSTS=\"`echo $ELASTICSEARCH_HOSTS | sed -e 's/ //g' | sed -e 's/,/", "/g'`\"
 
 # Render config file
 cat metricbeat.yml | sed "s|LOGSTASH_HOSTS|${LOGSTASH_HOSTS}|g" | sed "s|ELASTICSEARCH_HOSTS|${ELASTICSEARCH_HOSTS}|g" > metricbeat-out.yml
